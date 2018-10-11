82af498cfbd913ba15a200dbf8432c8e8a305b7b
naresh kumar <mnaresh2010@aol.com>
2018-10-11 10:49:40 +0530

    Create index.test.js

---
+++ src/index.test.js
@@ -0,0 +1,57 @@
+const { createStore } = require('redux');
+const appReducers = require('.');
+const should = require('chai').should();
+
+describe('Testnin reducers', function() {
+
+  it('should GET_USERNAME', function() {
+    const currState = {
+        username: ''
+    };
+
+    const store = createStore(appReducers, currState);
+
+    const action = {
+      type: 'GET_USERNAME',
+      username : 'naresh'
+    };
+
+    store.dispatch(action);
+    store.getState().should.have.property('username').and.equal('naresh');
+       store.getState().should.have.property('screen').and.equal('EntryScreen');
+  });
+
+  it('should SET_USERNAME', function() {
+
+    const currState = {
+        username: ''
+    };
+
+    const store = createStore(appReducers, currState);
+
+    const action = {
+      type: 'SET_USERNAME',
+      username : 'naresh'
+    };
+
+    store.dispatch(action);
+    store.getState().should.have.property('username').and.equal('naresh');
+    store.getState().should.have.property('screen').and.equal('ChatScreen');
+  });
+
+  it('should LOGOUT_USER', function() {
+    const currState = {
+        username: ''
+    };
+
+    const store = createStore(appReducers, currState);
+
+    const action = {
+      type: 'LOGOUT_USER'
+    };
+
+    store.dispatch(action);
+    store.getState().should.have.property('username').and.equal('');
+    store.getState().should.have.property('screen').and.equal('');
+  });
+})
